c77c87d35b33098db09ee06877393f31d4565aaf
yusu <tlsdbtn0507@naver.com>
2023-10-31 18:09:36 +0900

    [feature] add resizable

--- src/components/goorm/goalInput.js
+++ src/components/goorm/goalInput.js
@@ -1,26 +1,43 @@
 import css from '../../css/timeline.module.css'
 import { useState } from 'react'
-
+import { Resizable } from 're-resizable';
 
 const GoalInput = () => {
 
     const [goal,setGoals] = useState(null);
-    const [showGoal, setShowGoal] = useState(false)
+    const [opacity, setOpacity] = useState(0);
+    const [showGoal, setShowGoal] = useState(false);
+
+
     
     const submitGoalName = e => {
         e.preventDefault();
         const {value} = e.target[0]
-        setGoals(value);
+
+        
+        console.log(value.split(','))
+        setGoals(value.split(',')[0]);
+        setOpacity(+value.split(',')[1])
         setShowGoal(!showGoal)
+
+    }
+
+    const handleResize = ( a, b, ref ) => {
+        console.log(ref.style.width)
     }
     
     return(
         <tr className={css.cells}>
             <td>
                 { showGoal ? 
-                <div className={css.span} style={{opacity:1}}>{goal}</div> : 
+                <Resizable 
+                    onResize={handleResize}
+                    className={ opacity === 0 ? css.zeroSpan : css.span} 
+                    style={{backgroundColor:`rgba(147, 147, 160, ${opacity/100})`}}>
+                        {goal}
+                </Resizable> : 
                 <form  onSubmit={submitGoalName}>
-                    <input type='text'/>
+                    <input type='text' placeholder='골 이름 , 완료율'/>
                 </form>
                 }
             </td>
